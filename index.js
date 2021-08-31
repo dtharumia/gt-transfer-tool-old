cad92ec3f1bf45baa21261f618df1c0e3cf9ef83
dtharumia <senddharshan@gmail.com>
2021-08-30 23:15:08 -0400

    figured out general logic

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Courses = require('./models/courses')
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/gt-transfer-tool');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', ()=> {
+db.once('open', () => {
     console.log('Database connected')
 })
 
-const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req,res)=> {
+app.get('/', async (req, res) => {
     res.render('home')
+})
+
+app.post('/', async (req, res) => {
+    gt_course = req.body.class
+    const courses = await Courses.find({ 'gt_class': gt_course });
+    // res.render('home', { courses })
+    console.log({ courses })
+    res.render('courses', { courses })
 })
 
 app.listen(3000, () => {
